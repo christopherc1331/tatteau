de6502dfceda0bea5e0e0bf30435d40b5382e31b
Christopher Cruz <christopherc1331@gmail.com>
2025-04-18 01:07:09 -0500

    one last refactor for old crate name in web

--- web/src/app.rs
+++ web/src/app.rs
@@ -45,7 +45,7 @@ pub fn App() -> impl IntoView {
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/fo.css"/>
+        <Stylesheet id="leptos" href="/pkg/web.css"/>
 
         // sets the document title
         <Title text="Welcome to Leptos"/>
